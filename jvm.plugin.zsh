72c848c4537c6e22962c5f0dbbe63d5c62bacc5b
mgryszko <marcin@grysz.com>
2015-01-20 14:26:01 +0100

    Port from local oh-my-zsh installation

---
+++ jvm.plugin.zsh
@@ -0,0 +1,7 @@
+alias java_ls='/usr/libexec/java_home -V 2>&1 | grep -E "\d.\d.\d_\d\d" | cut -d , -f 1 | colrm 1 4 | grep -v Home'
+
+function java_use() {
+    export JAVA_HOME=$(/usr/libexec/java_home -v $1)
+    export PATH=$JAVA_HOME/bin:$PATH
+    java -version
+}
